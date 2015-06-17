18d3ff9d11994b4dd0546f4646f86aac1757f86f
charanbansal <linktocharan@gmail.com>
2015-06-18 00:12:06 +0530

    main data

---
+++ faq/O6SOu9hYsPHTU4.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="../theme/js/jquery-1.11.3.min.js" />
+
+var vw = $(window).width();
+var vh = $(window).height();
+var vid = document.getElementById("exploride-video");
+
+$(window).load(function () {
+    $(".preloader").fadeOut("slow");
+    setTimeout(function () {
+       
+    }, 1000);
+});
+$(document).ready(function () {
+    $("html").niceScroll({
+        cursorcolor: '#ed513d',
+        cursorborder: "1px solid transparent",
+        zindex: '1',
+        horizrailenabled:false,
+    });
+    $('.menu_toggler_md').click(function () {
+        $('#menu').slideToggle();
+    });
+    $('.q_a_cont:first-child .a_cont').slideDown("slow");
+
+    $('.q_cont i').click(function () {
+        $('.q_cont i').removeClass('active');
+        $(this).addClass('active');
+        $('.q_a_cont .a_cont').slideUp("slow");
+        $(this).parent('.q_cont').parent('.q_a_cont').children('.a_cont').slideDown();
+    });
+
+});
